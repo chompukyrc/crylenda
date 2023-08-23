9a6deeecb24b2af76c522acc069a710fe1b1a7d4
Kanyarat Chaiyanboon <kanyaratchaiyanboon527@gmail.com>
2023-08-23 21:49:26 +0700

    update

--- src/renderer/screens/GreetingScreen.vue
+++ src/renderer/screens/GreetingScreen.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-container class="flex justify-center items-center h-screen">Greeting</v-container>
+    <v-container class="flex justify-center items-center h-screen text-white text-3xl">Hihihihi</v-container>
 </template>
 
 <script lang="ts"></script>
