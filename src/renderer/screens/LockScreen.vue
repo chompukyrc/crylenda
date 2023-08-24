afdd0c36083bcc777c2ba0df50bf261067d6e36e
Thana Tingprasom <kookza01@gmail.com>
2023-08-24 19:04:47 +0700

    Update Color

--- src/renderer/screens/LockScreen.vue
+++ src/renderer/screens/LockScreen.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container class="flex justify-center items-center h-screen">
-        <v-otp-input variant="solo" type="password" focus-all> </v-otp-input>
+        <v-otp-input variant="solo" type="password" focus-all class=""> </v-otp-input>
     </v-container>
 </template>
 
