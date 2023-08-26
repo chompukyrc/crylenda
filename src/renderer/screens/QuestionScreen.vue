ffef3c9089927b017ef76c76ceeef53e7ea66b6f
Kanyarat Chaiyanboon <kanyaratchaiyanboon527@gmail.com>
2023-08-26 23:48:49 +0700

    fix button toggle

--- src/renderer/screens/QuestionScreen.vue
+++ src/renderer/screens/QuestionScreen.vue
@@ -1,21 +1,69 @@
 <template>
-    <v-container class="flex justify-center items-center h-screen text-white text-2xl flex-col animate-in zoom-in duration-1000">
-        <div>Have you cried today?</div>
+    <v-container
+        class="flex justify-center items-center h-5/6 text-white text-2xl flex-col animate-in zoom-in duration-1000"
+    >
+        <div>Have you cried today? {{ selected }}</div>
         <v-container>
             <v-row justify="center">
                 <v-col cols="auto">
-                    <router-link to="/question2"
-                        ><v-btn elevation="8" height="120" min-width="200" color="white"> Cry </v-btn></router-link
+                    <v-btn
+                        :class="`${selected === 0 ? ' bg-red-300  ' : '  bg-white '}`"
+                        elevation="8"
+                        height="120"
+                        min-width="200"
+                        @click="selected = 0"
                     >
+                        {{ options[0] }}
+                    </v-btn>
                 </v-col>
                 <v-col cols="auto">
-                    <router-link to="/diary"
-                        ><v-btn elevation="8" height="120" min-width="200" color="white"> not cry </v-btn></router-link
+                    <v-btn
+                        :class="`${selected === 1 ? ' bg-red-300  ' : '  bg-white '}`"
+                        elevation="8"
+                        height="120"
+                        min-width="200"
+                        @click="selected = 1"
                     >
+                        {{ options[1] }}
+                    </v-btn>
                 </v-col>
             </v-row>
         </v-container>
     </v-container>
+    <div class="flex justify-end items-center mx-20 h-1/6">
+        <v-btn
+            icon="mdi-arrow-right"
+            size="large"
+            color="white"
+            variant="tonal"
+            :disabled="selected === -1"
+            @click="nextHandler()"
+        ></v-btn
+    ></div>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+export default {
+    data: () => ({
+        selected: -1,
+        options: ["Cry", "Not Cry"]
+    }),
+    methods: {
+        // cryHandler(message: string) {
+        //     this.selected = message
+        //     console.log(message)
+        // },
+        // noCryHandler(message: string) {
+        //     this.selected = message
+        //     console.log(message)
+        // },
+        nextHandler() {
+            if (this.selected === "cry") {
+                this.$router.push("/question2")
+            } else {
+                this.$router.push("/diary")
+            }
+        }
+    }
+}
+</script>
